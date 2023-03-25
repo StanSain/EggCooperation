d9589c23fd9c6cf06e1d20300127da85a4479f42
StanSain <127401658+StanSain@users.noreply.github.com>
2023-03-25 11:50:37 -0300

    sopa de letras v 1

--- sopaDeLetras.java
+++ sopaDeLetras.java
@@ -10,24 +10,57 @@ Nota: Para resolver el ejercicio deberá investigar cómo se utilizan las
 siguientes funciones de Java substring(), Length() y Math.random().
  */
 package eggcooperation;
+import java.util.
+import java.util.Scanner;
 
 public class sopaDeLetras {
 
     public static void main(String[] args) {
-
-        
-        
+        String[][] sopa = new String[20][20];//defino matriz sopa de 20x20
+        Inic(sopa);
+        String palabra = "";
+        for (int i = 0; i <= 4; i++) {
+            Valido();//lee palabra y valida su largo entre 3 yy 5
+LLeno(String[][]sopa,String palabra);
+        }
     }
     //-----------------
-    
-    
-    
-    
-    
-    
+
+    public static String Valido() {
+        Scanner leer = new Scanner(System.in);
+        String palabra = "";
+        System.out.println("ingrese palabra de entre tres y cinco caracteres");
+        palabra = leer.next();
+        int largo = palabra.length();
+        if ((largo < 3) || (largo > 5)) {
+            System.out.println("ingrese palabra de entre tres y cinco caracteres");
+            palabra = leer.next();
+        }
+        return palabra;
+    }
+
     //-------------------------------
+    public static void Inic(String[][] sopa) {
+        for (int i = 0; i <= 19; i++) {
+            for (int j = 0; j <= 19; i++) {
+                sopa[i][j] = "";
+            }
+        }
+    }
+
+    //---------------------------------
+   public static void LLeno(String[][]sopa,String palabra){
+       String letra="";
+       int largo = palabra.length();
+       for (int i = 0; i <= (largo-1); i++){
+           letra = palabra.substring(i, i+1);
+       }
+   }
+    
+
     
     
     
     
+    //-----------------------
 }
